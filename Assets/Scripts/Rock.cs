ffc98e7aced62ceaa033359000a58d37887b70d0
YoungMoo Kim <rladuan612@gmail.com>
2025-08-19 21:22:34 +0900

    add obstacle & audio source

---
+++ Assets/Scripts/Rock.cs
@@ -0,0 +1,48 @@
+using System;
+using Unity.Cinemachine;
+using UnityEngine;
+
+public class Rock : MonoBehaviour
+{   
+    [SerializeField] ParticleSystem collisionParticleSystem;
+    [SerializeField] AudioSource boulderSmashAudioSource;
+    [SerializeField] float shakeModifer = 10f;
+    [SerializeField] float collisionColldown = 1f;
+    CinemachineImpulseSource cinemachineImpulseSource;
+
+    float collisionTimer = 0f;
+
+    void Awake()
+    {
+        cinemachineImpulseSource = GetComponent<CinemachineImpulseSource>();
+    }
+
+    void Update()
+    {
+        collisionTimer += Time.deltaTime;
+    }
+
+    void OnCollisionEnter(Collision other)
+    {   
+        if(collisionTimer < collisionColldown) return;
+        FireImpulse();
+        CollisionFX(other);
+        collisionTimer = 0f;
+    }
+
+    void FireImpulse()
+    {
+        float distance = Vector3.Distance(transform.position, Camera.main.transform.position);
+        float shakeIntensity = (1f / distance) * shakeModifer;
+        shakeIntensity = MathF.Min(shakeIntensity, 1f);
+        cinemachineImpulseSource.GenerateImpulse(shakeIntensity);
+    }
+
+    void CollisionFX(Collision other)
+    {
+        ContactPoint contactPoint = other.contacts[0];
+        collisionParticleSystem.transform.position = contactPoint.point;
+        collisionParticleSystem.Play();
+        boulderSmashAudioSource.Play();
+    }
+}
